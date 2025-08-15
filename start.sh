1ab5b50db57ce44eb6998fd354dfff9663bd3a23
muhammadhosseinaramesh-art <muhammadhosseinaramesh@gmail.com>
2025-08-15 20:37:08 +0330

    start.sh

--- start.sh
+++ start.sh
@@ -6,9 +6,7 @@ cat > config.py <<EOF
 PORT = ${PORT:-443}
 USERS = { "${SECRET}" : 100 }
 AD_TAG = "${AD_TAG}"
-# بقیه تنظیمات پیش‌فرض (در صورت نیاز می‌توانید اضافه کنید)
 EOF
 
-# اجرای پروکسی (اسکریپت اصلی در repo alexbers معمولاً mtprotoproxy.py است)
-# اگر اسامی در repo شما متفاوت است این خط را اصلاح کن.
+# اجرای پروکسی
 exec python3 mtprotoproxy.py
